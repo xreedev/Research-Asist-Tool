29efc35bc2f4b0bccb373a25ef6f05c64821e699
sreedev <xreedev@gmail.com>
2024-04-08 20:40:43 +0530

    main updated , bugs fixed

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import dataProcesser as DP
 import TFIDF as TI
 import data
-import BART as SB
-import ppt_util as PPT
+import BART as BRT
+# import BART as SB
+
 
 ieee_paper_data = data.ieee_paper_data  # contents of scientific paper
 stop_words = data.stop_words  # this contains non-important terms to be removed
@@ -16,7 +17,7 @@ summarization_choice =0
 Summarized_data = TI.tfidfVectorise(classified_dataset, lines, data.stop_words, 0.5)
 
 
-print("nothing")
+print(BRT.bertSummarize_dict(Summarized_data))
 
 
 
