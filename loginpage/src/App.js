b454e9acaa83556d63937cece5572192ac1f0d84
sharon <shxxrz97@gmail.com>
2024-04-13 18:48:07 +0530

    added login page of aisummarizer app

---
+++ loginpage/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import LoginForm from "./components/Login/login";
+function App() {
+  return (
+    <div className="App">
+      <LoginForm/>
+    </div>
+  );
+}
+
+export default App;
