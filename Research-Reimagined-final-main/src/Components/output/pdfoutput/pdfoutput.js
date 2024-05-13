2e92cc452634b9f4f92b160e27ffcf3c55714363
Sreedev TS <xreedev@gmail.com>
2024-05-13 15:14:32 +0530

    added files via upload

---
+++ Research-Reimagined-final-main/src/Components/output/pdfoutput/pdfoutput.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+
+function PDFOutput({ summarizedText }) {
+  return (
+    <Container fluid>
+      <Row className="justify-content-center">
+        <Col xs={12}>
+          <Card style={{ height: '400px' }}>
+            <Card.Header style={{ fontWeight: 'bold' }}>PDF Summary</Card.Header>
+            <Card.Body style={{ maxHeight: '350px', overflowY: 'auto' }}>
+              <p>{summarizedText}</p>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default PDFOutput;
